361e403e677cfb6d70302dba2f26f4605213f841
MikaelDv <enzomikael@hotmail.com>
2024-08-15 12:15:36 -0300

    first commit

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,29 @@
+import "./Calculator.css";
+
+export default function Calculator() {
+  return (
+    <>
+      <div className="wrapper">
+        <button>AC</button>
+        <button>+/-</button>
+        <button>%</button>
+        <button>÷</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>X</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+        <button id="butaoZero">0</button>
+        <button>,</button>
+        <button>=</button>
+      </div>
+    </>
+  );
+}
